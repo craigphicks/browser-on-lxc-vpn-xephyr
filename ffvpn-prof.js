5cbe4280bcce6743eb6f197feffba7c7706a9c14
craigphicks <13205732+craigphicks@users.noreply.github.com>
2020-04-01 12:25:26 -0700

    intiial code commit

---
+++ ffvpn-prof.js
@@ -0,0 +1,113 @@
+'strict'
+
+const util = require('util')
+const { execSync, exec } = require('child_process')
+//exec = util.promisify(exec)
+const fs = require('fs')
+const yaml = require('js-yaml');
+
+
+function syscmd(cmd) {
+	var so=''
+	try {
+		so = execSync(cmd, { encoding: 'utf-8' })
+		//  console.log("SUCCESS")
+		return so
+	} catch (e) {
+		console.log('command fail:\n', cmd)
+		console.log('command output:\n', so)
+		console.log('command error:\n', e)
+		throw e
+	}
+}
+
+
+function getContainerIp4AddressFromListJSON(json,name){
+	// var aa = json.find(c=>c.name==name).state.network.eth0.addresses
+	// for (a of aa)
+	// 	console.log(a)
+	return json.find(c=>c.name==name)
+		.state.network.eth0.addresses.find(a=>a.family=='inet')
+		.address
+}
+	
+
+async function createProfile(
+	copyProfileName,
+	profileName,
+	cloudInitJSON,
+	authorizedPubKeyFilename,
+	phoneHomeURL) {
+
+	//function getFileText(filename) {
+	//	let t = fs.readFileSync(filename, 'utf8')
+	//	return t
+	//}
+
+
+	// embed ssh public key which will be used to send into the ffvpn container
+	cloudInitJSON.ssh_authorized_keys= [
+		fs.readFileSync(authorizedPubKeyFilename, "utf8")
+	]
+	cloudInitJSON.package_upgrade = true
+	cloudInitJSON.phone_home = {
+	 	"url": phoneHomeURL,
+	 	"post": "all",
+	 	"tries": 10
+	}
+
+	// convert the cloud init instructions to yaml format, which is a string.
+	var cldinit_yml = yaml.safeDump(cloudInitJSON)
+	// CRITICAL!!!! The first line of the yaml must be "#cloud-config"
+	// It's not just a pretty comment!
+	cldinit_yml = "#cloud-config\n" + cldinit_yml
+	//console.log(cldinit_yml)
+
+	// check if new profile name already exist and delete it if so.
+	var profs = syscmd('lxc profile list --format yaml')
+	profs = yaml.safeLoad(profs)
+	for (p of profs) {
+		//console.log(p)
+		//console.log(p.name)
+		if (p.name == profileName) 
+			syscmd('lxc profile delete ' + profileName)
+	}
+	
+	// create the new profile as a copy of the default profile
+	syscmd('lxc profile copy default ' + profileName)
+	var prof_ffvpn_yml=syscmd('lxc profile show '+ profileName)
+	var prof_ffvpn_json = yaml.safeLoad(prof_ffvpn_yml)
+
+	// the cloud init data is added as a string of yaml to json, confusing but true.
+	// So when the whole is converted to yaml cloudinit should be a string, not yaml
+	prof_ffvpn_json["config"]["user.user-data"]=cldinit_yml
+
+	prof_ffvpn_json.description = "profile for ffvpn project"
+
+	//console.log("after assignment of cloud init data\n", JSON.stringify(prof_ffvpn_json))
+
+	// now we write it back out as a new lxc profile using the lxc edit call
+
+	await new Promise((resolve, reject)=>{
+		
+		var proc = exec('lxc profile edit ' + profileName, (error, stdout, stderr) => {
+			if (error) {
+				reject(`edit error: ${error}`)
+			}
+			//console.log(`edit success, stdout:\n ${stdout}`);
+			//console.log(syscmd('lxc profile show ' + profileName))
+			//  console.error(`stderr: ${stderr}`);
+			resolve()
+		});
+
+		// write the profile data to stdin
+		if (!proc.stdin.write(JSON.stringify(prof_ffvpn_json)))
+			reject('edit error, stdin write failed')
+		proc.stdin.end()
+	})					 
+}
+
+
+exports.createProfile =  createProfile
+exports.syscmd = syscmd
+exports.getContainerIp4AddressFromListJSON = getContainerIp4AddressFromListJSON
---
+++ index.js
@@ -0,0 +1,177 @@
+'strict'
+
+const fs = require('fs')
+const url = require('url')
+const http = require('http')
+const { parse } = require('querystring');
+const yaml = require('js-yaml');
+
+const { createProfile, syscmd, getContainerIp4AddressFromListJSON }
+	  = require('./ffvpn-prof.js')
+
+
+
+const sshKeyFilename = '/home/craig/.ssh/to-ffvpn'
+const vpnClientCertFilename = '/home/craig/ffvpn-dev/ffvpn-client1.ovpn'
+const lxcImageSrc = `ubuntu:18.04`
+const lxcImageAlias = `ub18-im`
+const lxcContName = `ub18-ffvpn`
+const lxcCopyProfileName = 'default'
+const lxcProfileName = 'ffvpn-prof'
+const lxcContBridgeName = 'lxdbr0'
+const phoneHomePort = 3000
+//const cloudInitPhoneHomeIP4 = "10.64.64.1"
+//const cloudInitPhoneHomeURL = `http://${cloudInitPhoneHomeIP4}:${cloudInitPhoneHomePort}`
+
+// package_upgrade: true, ssha_authorized_keys: ...
+// will added internally
+var cloudInitJSON = {
+	"locale": "en_US.UTF-8", 
+	"timezone": "America/Los_Angeles", 
+	"packages": [
+	 	"firefox", 
+	 	"openvpn"
+	], 
+	"runcmd": [
+		[
+			"touch", 
+			"/home/ubuntu/iwozere"
+		]
+	]
+}
+
+async function main(){
+	// create an instance of a container from that image
+	// check name already 
+	var clist = JSON.parse(syscmd(`lxc list --format json`))
+	//console.log(clist)
+	for (c of clist) {
+		if (c.name==lxcContName)
+			//throw `container ${lxcContName} already exists`
+			syscmd(`lxc delete --force ${lxcContName}`) 
+		//console.log(c.name, ' !== ', lxcContName) 
+	}
+	
+	// create key
+	if (!(fs.existsSync(`${sshKeyFilename}.pub`) &&
+		  fs.existsSync(`${sshKeyFilename}`))) {
+		if (fs.existsSync(`${sshKeyFilename}.pub`))
+			fs.unlinkSync(`${sshKeyFilename}.pub`)
+		if (fs.existsSync(`${sshKeyFilename}`))
+			fs.unlinkSync(`${sshKeyFilename}`)
+		syscmd(`ssh-keygen -f ${sshKeyFilename} -N ''`)
+	}
+	if (!(fs.existsSync(`${sshKeyFilename}.pub`) &&
+		  fs.existsSync(`${sshKeyFilename}`))) {
+		throw `failed to create ssh key ${sshKeyFilename}`
+	}
+
+	console.log("KEY done ...")
+
+	// determine cloud init phone_home post destination and ufw rule to allow it
+	// (depends on bridge ${lxcContBridgeName})
+	//console.log(yaml.safeLoad(syscmd(`lxc network show ${lxcContBridgeName}`)))
+	let phoneHomeFromCDN = yaml.safeLoad(syscmd(`lxc network show ${lxcContBridgeName}`)).
+		config['ipv4.address']
+	let phoneHomeToAddr = phoneHomeFromCDN.split('/')[0]
+	let phoneHomeURL = `http://${phoneHomeToAddr}:${phoneHomePort}`
+
+	// create profile
+	await createProfile(
+		lxcCopyProfileName,
+		lxcProfileName,
+		cloudInitJSON,
+		`${sshKeyFilename}.pub`,
+		phoneHomeURL
+	)
+
+	console.log("PROFILE done ...")
+	
+	// copy the lxc image (if already exists does not fail)
+	if (JSON.parse(syscmd(`lxc image list ${lxcImageAlias} --format json`)).length==0) {
+		syscmd(`lxc image copy ${lxcImageSrc} local: --alias ${lxcImageAlias}`)
+	}
+	console.log("IMAGE done ...")
+
+	let ufwRuleCmd =
+		`sudo ufw allow from ${phoneHomeFromCDN} to ${phoneHomeToAddr} ` 
+		+ `port ${phoneHomePort} proto tcp`;
+	console.log(`ADDING UFW RULE for phome_home:\n${ufwRuleCmd}`)
+	console.log(syscmd(ufwRuleCmd))
+	
+	// set up receiver for cloud init phone home signalling cloud init end
+	var promPhoneHome = waitPhoneHome(phoneHomeToAddr, phoneHomePort)
+
+	// create the container with cloud init customization
+	syscmd(`lxc launch ${lxcImageAlias} ${lxcContName} -p ${lxcProfileName}`)
+
+	console.log("LAUNCH executed, waiting for phone home to signal cloud init finished ...")
+
+	await promPhoneHome;
+
+	// wait until cloud init has finished
+	console.log("CONTAINER has finished cloud init")
+
+	var contip4 = getContainerIp4AddressFromListJSON(
+		JSON.parse(syscmd(`lxc list --format json`)), lxcContName
+	)
+
+	console.log("CONTAINER ip4 address is " + contip4)
+}
+
+
+async function waitPhoneHome(phoneHomeToAddr, phoneHomePort){
+	return new Promise( (resolve, reject) => {
+
+		function collectRequestData(request, callback) {
+			const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+			if(request.headers['content-type'] === FORM_URLENCODED) {
+				let body = '';
+				request.on('data', chunk => {
+					body += chunk.toString();
+				});
+				request.on('end', () => {
+					callback(parse(body));
+				});
+			}
+			else {
+				callback(null);
+			}
+		}		
+		///var server=null;
+		const requestHandler = (request, response) => {
+			console.log(request.method)
+			if (request.method === 'POST') {
+				collectRequestData(request, result => {
+					//console.log("phone_home result:\n:", result);
+					console.log("phone_home signal received");
+					response.end();
+					server.close();
+					resolve(result);
+				})
+			}
+        }
+		
+		const server = http.createServer(requestHandler)
+
+		server.listen(phoneHomePort, phoneHomeToAddr, (err) => {
+			if (err) {
+				reject('waitPhoneHome server.listen callback, ERROR:\n' + err)
+			}
+			//console.log(`waitPhoneHome server.listen callback, not error but unexpected`)
+		})
+	})
+}
+	
+main()
+	.then(()=>{
+		process.exitCode=0
+		console.log("SUCCESS")
+	})
+	.catch(e => {
+		process.exitCode=1
+		console.log("FAIL",e)
+	})	
+	.finally(()=>{
+		console.log("EXIT")
+	})
